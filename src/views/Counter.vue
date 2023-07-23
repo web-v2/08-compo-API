9a1110d3b1c33c55bdd0541c3d9a0ebf38b15751
Samir Vergara V <svergara3@misena.edu.co>
2023-07-23 14:55:45 -0500

    Cargue Inicial 2023-07-23

---
+++ src/views/Counter.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="counter">
+      <h1>This is an Counter page</h1>
+      <h2>{{nameCount}}</h2>
+
+      <button @click="decrease">-</button>
+      <button @click="increase">+</button>
+    </div>
+  </template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+    name: 'Counter',
+    props: {},
+    emies: [],
+    setup() {
+        const nameCount = ref(18);
+        /*const increase = () =>{
+            nameCount.value++
+        }
+        const decrease = () =>{
+            nameCount.value--
+        }*/
+        return {
+            nameCount,
+            
+            increase: () => nameCount.value++,
+            decrease: () => nameCount.value--
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <div class="about">
+      <h1>This is an Home page</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView'
+  }
+</script>
